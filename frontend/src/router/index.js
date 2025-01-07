23b8996a192aa3693d35312fa4f3fa8e51902379
John Adams <johnmadams96@gmail.com>
2025-01-07 12:16:31 -0500

    chore: setup routes and 404 page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,40 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import DashBoard from "../views/DashBoardView.vue";
+import FourView from "../views/FourView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "dashboard",
+    component: DashBoard,
+  },
+  {
+    path: "/:catchAll(.*)",
+    component: FourView,
+    name: "NotFound",
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
+  routes,
+});
 
-export default router
+// Add a global beforeEach guard
+router.beforeEach((to, from, next) => {
+  const isPageReload = sessionStorage.getItem("isPageReload");
+  sessionStorage.removeItem("isPageReload");
+
+  if (isPageReload && to.fullPath !== "/") {
+    next("/");
+  } else {
+    next();
+  }
+});
+
+// Set a flag to detect page reload
+window.addEventListener("beforeunload", () => {
+  sessionStorage.setItem("isPageReload", "true");
+});
+
+export default router;
